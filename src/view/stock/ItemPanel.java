b4b3c780fe7d1efd29aa4b898607ab2d520688d0
Laureen Ginier <ginier.laureen@etu.unice.fr>
2014-04-27 19:22:18 +0200

    Retrait des marges et gestion des chiffres après la virgule

--- src/view/stock/ItemPanel.java
+++ src/view/stock/ItemPanel.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.DecimalFormat;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,11 +15,16 @@ import model.stock.Item;
 
 @SuppressWarnings("serial")
 public class ItemPanel extends JPanel {
+
     private Item item;
     private JTextField nbrMarch, nbrHorsMarch;
     private JLabel labelIntitule, labelPrix, labelQteMarch, labelValeur;
 
+    DecimalFormat df;
+
     public ItemPanel(Item item) {
+	df = new DecimalFormat("0.00");
+	df.setMinimumFractionDigits(2); // nb de chiffres apres la virgule
 	this.item = item;
 	this.nbrMarch = new JTextField(String.valueOf(item.getNbrMarch()));
 	this.nbrMarch.setHorizontalAlignment(JTextField.CENTER);
@@ -30,11 +36,11 @@ public class ItemPanel extends JPanel {
 		JLabel.CENTER);
 	this.labelQteMarch = new JLabel(
 		String.valueOf(item.getQuantiteMarch()), JLabel.CENTER);
-	this.labelValeur = new JLabel(String.valueOf(item.getValeur()),
+	this.labelValeur = new JLabel(df.format(item.getValeur()),
 		JLabel.CENTER);
 	this.nbrMarch.addKeyListener(new ClavierListener());
 	this.nbrHorsMarch.addKeyListener(new ClavierListener());
-	this.setLayout(new GridLayout(1, 6, 20, 10));
+	this.setLayout(new GridLayout(1, 6));
 	this.add(labelIntitule);
 	this.add(labelPrix);
 	this.add(labelQteMarch);
@@ -56,36 +62,32 @@ public class ItemPanel extends JPanel {
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-	    /*
-	     * if (!isNumeric(event.getKeyChar())) {
-	     * nbrMarch.setText(nbrMarch.getText().replace(
-	     * String.valueOf(event.getKeyChar()), "")); } else {
-	     */
 	    int nbMarch = 0;
 	    int nbHorsMarch = 0;
-	    if (!isNumeric(nbrMarch.getText()) || nbrMarch.getText().equals("")) {
-		nbMarch = 0;
-	    } else {
-		if (!isNumeric(nbrHorsMarch.getText())
-			|| nbrHorsMarch.getText().equals("")) {
-		    nbHorsMarch = 0;
-		} else {
-		    nbMarch = Integer.parseInt(nbrMarch.getText());
-		    nbHorsMarch = Integer.parseInt(nbrHorsMarch.getText());
+	    if (!isNumeric(nbrMarch.getText())
+		    || !isNumeric(nbrHorsMarch.getText())) {
+		labelValeur.setText("0");
+		if (!isNumeric(nbrMarch.getText())) {
+		    // nbrMarch.setText("");
 		}
+		if (!isNumeric(nbrHorsMarch.getText())) {
+		    // nbrHorsMarch.setText("");
+		}
+	    } else {
+		nbMarch = Integer.parseInt(nbrMarch.getText());
+		nbHorsMarch = Integer.parseInt(nbrHorsMarch.getText());
+		item.setNbrMarch(nbMarch);
+		item.setNbrHorsMarch(nbHorsMarch);
+		Double valeurPaquetPlein = item.getPrix() * nbMarch;
+		Double valeurPaquetEntame = nbHorsMarch * item.getPrix()
+			/ item.getQuantiteMarch();
+		Double calcul = valeurPaquetPlein + valeurPaquetEntame;
+		labelValeur.setText(String.valueOf(df.format(calcul)));
 	    }
-	    item.setNbrMarch(nbMarch);
-	    item.setNbrHorsMarch(nbHorsMarch);
-	    Double valeurPaquetPlein = item.getPrix() * nbMarch;
-	    Double valeurPaquetEntame = nbHorsMarch * item.getPrix()
-		    / item.getQuantiteMarch();
-	    Double calcul = valeurPaquetPlein + valeurPaquetEntame;
-	    labelValeur.setText(String.valueOf(calcul));
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-
 	    // TODO Auto-generated method stub
 
 	}
@@ -102,6 +104,7 @@ public class ItemPanel extends JPanel {
 	}
     }
 
+    // TODO Pour tester, à supprimer
     public static void main(String[] args) {
 	Item item = new Item();
 	item.setIntitule("Carotte");
@@ -115,11 +118,9 @@ public class ItemPanel extends JPanel {
 	pane.add(new ItemPanel(item));
 	frame.setContentPane(pane);
 	frame.pack(); // Adapte la fenêtre au contenu
-	frame.setMinimumSize(new Dimension(250, 65)); // Donne une taille
-						      // minimale à la fenêtre
+	frame.setMinimumSize(new Dimension(250, 65));
 	frame.setVisible(true);
 	frame.setLocationRelativeTo(null); // Centre la fenêtre
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
 }
